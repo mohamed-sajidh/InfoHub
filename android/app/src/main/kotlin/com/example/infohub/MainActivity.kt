d0e0a67a9b6c195a61678e96a466fbcee8dcb28b
sajidh <sajidhshaji4@gmail.com>
2024-07-09 12:50:47 +0530

    Folder Structure Created

---
+++ android/app/src/main/kotlin/com/example/infohub/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.infohub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/feature/landing_page/landing_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 50,
+      color: Colors.red,
+    );
+  }
+}
